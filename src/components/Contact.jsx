9c5cf5a29f9afcee63cf90bd96750cb07fc94718
TedXpro <johannes.woldeyes@gmail.com>
2025-12-07 18:47:52 +0300

    modify the UI for send button and hero page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -115,7 +115,7 @@ const Contact = () => {
 
           <button
             type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            className='bg-[#915EFF] py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md hover:bg-[#7d4cca] transition-colors'
           >
             {loading ? "Sending..." : "Send"}
           </button>
